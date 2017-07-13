d46f40522cbde9ebb90b6a2ab7a90d4242c4802b
BrianJenney <brianjenney83@gmail.com>
2017-07-12 21:07:11 -0700

    react starter with single component, web pack and enough to get started

---
+++ react-starter/components/App.js
@@ -0,0 +1,36 @@
+//we must import react to use it in our app
+import React from 'react';
+
+//this is our component which extends our react object
+class App extends React.Component {
+
+  //this is a function allows us to access the parents props or state
+  //don't worry, we will cover state later
+
+  //just know you will need to call this in order to access props or set state
+  constructor(props){
+    super(props)
+
+    //state is our data being held in this component
+    //in this case it will be our greeting
+    this.state={greeting: 'Hello'};
+  }
+
+  //this is our render function where the action takes place!
+  //this function returns some jsx (think javascript xml)
+  //we put our variables in a curly bracket just like we do with angular
+  //but with one bracket instead of two... because react!
+   render() {
+      return (
+         <div>
+            {this.state.greeting}
+         </div>
+      );
+   }
+}
+
+//we export our component so we can use it anywhere else in the App
+//this modularity is what makes react so appealing to devs:
+//you can write a presentational component and re-use it anywhere without
+//reinventing the wheel
+export default App;
---
+++ react-starter/webpack.config.js
@@ -0,0 +1,23 @@
+var config = {
+   entry: './index.js', //our point of entry that renders our react components
+   output: {
+      path:'/public',
+      filename: 'bundle.js', //here is where our jsx will be transpiled to actual js that can live in our browser
+   },
+   devServer: {
+      inline: true, //this allows our changes to take place immediately without refreshing
+      port: 8080 //the port where our app will be served from
+   },
+   module: {
+       loaders: [
+         {
+           test: /\.js?$/,
+           exclude: /(node_modules|bower_components)/,
+           loaders: [
+             'babel-loader?presets[]=react,presets[]=es2015,presets[]=stage-2' //babel is what transpiles our jsx code into javascript
+           ]
+         }
+       ]
+     }
+}
+module.exports = config;
