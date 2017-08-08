a7861da479f4de20a775eeadf0a1ac878141b081
BrianJenney <brianjenney83@gmail.com>
2017-08-08 07:28:44 -0700

    react routes project

---
+++ react-routes-node/components/main.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+import SearchBar from './searchBar';
+
+import '../styles/master.scss';
+
+class Main extends React.Component {
+
+   render() {
+      return (
+         <div>
+           <SearchBar/>
+         </div>
+      );
+   }
+}
+
+export default Main;
