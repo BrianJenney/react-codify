8b91ca20f17c63c67989ba03ecf609c0d2d02b64
BrianJenney <brianjenney83@gmail.com>
2017-08-12 12:47:50 -0700

    react routes with node backend

---
+++ react-routes-node/components/child.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../styles/master.scss';
+
+class Child extends React.Component {
+
+   render() {
+      return (
+         <div>
+           <h1>Child Route</h1>
+         </div>
+      );
+   }
+}
+
+export default Child;
--- react-routes-node/index.js
+++ react-routes-node/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { HashRouter as Router, Route } from 'react-router-dom';
 //we import our component from our component folder to use here
 import Main from './components/main';
+import Child from './components/child';
 
 //using the react-dom render method, we access our html and
 //attach our react app at this connection point
-ReactDOM.render(<Main />, document.getElementById('app'));
+
+
+ReactDOM.render(
+  <Router>
+    <div>
+       <Route exact path="/" component={Main} />
+       <Route path="/about" component={Child} />
+     </div>
+  </Router >,
+  document.getElementById('app')
+);
