b8749cb1ca1e0a4715103b16435311b6057fd3e2
BrianJenney <brianjenney83@gmail.com>
2017-08-31 10:19:15 -0700

    react template

---
+++ react_template/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//using the react-dom render method, we access our html and
+//attach our react app at this connection point
+ReactDOM.render(<YourElementHere/>, document.getElementById('app'));
